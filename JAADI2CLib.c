d0369a6d6ec5224a6c161b9624b8b299f1a15fa2
aaroexxt <aaron.becker@gmail.com>
2019-07-22 21:43:43 -0700

    i2c beginning

--- JAADI2CLib.c
+++ JAADI2CLib.c
@@ -1,6 +1,24 @@
 /* 
- * File:   JAADSM
+ * File:   JAADI2CLib.c
  * Author: Aaron Becker
  *
  * Created on July 5, 2019, 1:32 PM
  */
+
+#include "JAADI2CLib.h"
+#include "JAADIOLib.h"
+
+char I2C_initted = FALSE;
+
+int I2C_Init(unsigned int speed) {
+	if (!I2C_initted) {
+		I2C_initted = true;
+		//Set SCL/SDA to OUTPUTS
+		//Set SCL/SDA to HIGH
+		if (IO_checkLAT(SCLLAT))
+
+	}
+}
+void internal_startSeq(char I2Caddress) {
+
+}
