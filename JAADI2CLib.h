009989935ad8a64283606f331bb453138e541c91
aaroexxt <aaron.becker@gmail.com>
2019-07-23 15:07:28 -0700

    errors fixed

--- JAADI2CLib.h
+++ JAADI2CLib.h
@@ -7,7 +7,6 @@
 
 #ifndef JAADSM_H
 #define	JAADSM_H
-#include "JAADIOLib.h";
 
 #ifdef	__cplusplus
 extern "C" {
@@ -38,99 +37,101 @@ extern "C" {
 
 #define SENSORS_GRAVITY_STANDARD             (9.80665F)              /**< Earth's gravity in m/s^2 */
 
-enum {
-	LSM9DS1_REGISTER_WHO_AM_I_XG         = 0x0F,
-	LSM9DS1_REGISTER_CTRL_REG1_G         = 0x10,
-	LSM9DS1_REGISTER_CTRL_REG2_G         = 0x11,
-	LSM9DS1_REGISTER_CTRL_REG3_G         = 0x12,
-	LSM9DS1_REGISTER_TEMP_OUT_L          = 0x15,
-	LSM9DS1_REGISTER_TEMP_OUT_H          = 0x16,
-	LSM9DS1_REGISTER_STATUS_REG          = 0x17,
-	LSM9DS1_REGISTER_OUT_X_L_G           = 0x18,
-	LSM9DS1_REGISTER_OUT_X_H_G           = 0x19,
-	LSM9DS1_REGISTER_OUT_Y_L_G           = 0x1A,
-	LSM9DS1_REGISTER_OUT_Y_H_G           = 0x1B,
-	LSM9DS1_REGISTER_OUT_Z_L_G           = 0x1C,
-	LSM9DS1_REGISTER_OUT_Z_H_G           = 0x1D,
-	LSM9DS1_REGISTER_CTRL_REG4           = 0x1E,
-	LSM9DS1_REGISTER_CTRL_REG5_XL        = 0x1F,
-	LSM9DS1_REGISTER_CTRL_REG6_XL        = 0x20,
-	LSM9DS1_REGISTER_CTRL_REG7_XL        = 0x21,
-	LSM9DS1_REGISTER_CTRL_REG8           = 0x22,
-	LSM9DS1_REGISTER_CTRL_REG9           = 0x23,
-	LSM9DS1_REGISTER_CTRL_REG10          = 0x24,
+#define	LSM9DS1_REGISTER_WHO_AM_I_XG          0x0F
+#define	LSM9DS1_REGISTER_CTRL_REG1_G          0x10
+#define	LSM9DS1_REGISTER_CTRL_REG2_G          0x11
+#define	LSM9DS1_REGISTER_CTRL_REG3_G          0x12
+#define	LSM9DS1_REGISTER_TEMP_OUT_L           0x15
+#define	LSM9DS1_REGISTER_TEMP_OUT_H           0x16
+#define	LSM9DS1_REGISTER_STATUS_REG           0x17
+#define	LSM9DS1_REGISTER_OUT_X_L_G            0x18
+#define	LSM9DS1_REGISTER_OUT_X_H_G            0x19
+#define	LSM9DS1_REGISTER_OUT_Y_L_G            0x1A
+#define	LSM9DS1_REGISTER_OUT_Y_H_G            0x1B
+#define	LSM9DS1_REGISTER_OUT_Z_L_G            0x1C
+#define	LSM9DS1_REGISTER_OUT_Z_H_G            0x1D
+#define	LSM9DS1_REGISTER_CTRL_REG4            0x1E
+#define	LSM9DS1_REGISTER_CTRL_REG5_XL         0x1F
+#define	LSM9DS1_REGISTER_CTRL_REG6_XL         0x20
+#define	LSM9DS1_REGISTER_CTRL_REG7_XL         0x21
+#define	LSM9DS1_REGISTER_CTRL_REG8            0x22
+#define	LSM9DS1_REGISTER_CTRL_REG9            0x23
+#define	LSM9DS1_REGISTER_CTRL_REG10           0x24
 
-	LSM9DS1_REGISTER_OUT_X_L_XL          = 0x28,
-	LSM9DS1_REGISTER_OUT_X_H_XL          = 0x29,
-	LSM9DS1_REGISTER_OUT_Y_L_XL          = 0x2A,
-	LSM9DS1_REGISTER_OUT_Y_H_XL          = 0x2B,
-	LSM9DS1_REGISTER_OUT_Z_L_XL          = 0x2C,
-	LSM9DS1_REGISTER_OUT_Z_H_XL          = 0x2D
+#define	LSM9DS1_REGISTER_OUT_X_L_XL           0x28
+#define	LSM9DS1_REGISTER_OUT_X_H_XL           0x29
+#define	LSM9DS1_REGISTER_OUT_Y_L_XL           0x2A
+#define	LSM9DS1_REGISTER_OUT_Y_H_XL           0x2B
+#define	LSM9DS1_REGISTER_OUT_Z_L_XL           0x2C
+#define	LSM9DS1_REGISTER_OUT_Z_H_XL           0x2D
 
-}
 
-enum {
-	LSM9DS1_REGISTER_WHO_AM_I_M         = 0x0F,
-	LSM9DS1_REGISTER_CTRL_REG1_M        = 0x20,
-	LSM9DS1_REGISTER_CTRL_REG2_M        = 0x21,
-	LSM9DS1_REGISTER_CTRL_REG3_M        = 0x22,
-	LSM9DS1_REGISTER_CTRL_REG4_M        = 0x23,
-	LSM9DS1_REGISTER_CTRL_REG5_M        = 0x24,
-	LSM9DS1_REGISTER_STATUS_REG_M       = 0x27,
-	LSM9DS1_REGISTER_OUT_X_L_M          = 0x28,
-	LSM9DS1_REGISTER_OUT_X_H_M          = 0x29,
-	LSM9DS1_REGISTER_OUT_Y_L_M          = 0x2A,
-	LSM9DS1_REGISTER_OUT_Y_H_M          = 0x2B,
-	LSM9DS1_REGISTER_OUT_Z_L_M          = 0x2C,
-	LSM9DS1_REGISTER_OUT_Z_H_M          = 0x2D,
-	LSM9DS1_REGISTER_CFG_M              = 0x30,
-	LSM9DS1_REGISTER_INT_SRC_M          = 0x31
-}
+#define	LSM9DS1_REGISTER_WHO_AM_I_M          0x0F
+#define	LSM9DS1_REGISTER_CTRL_REG1_M         0x20
+#define	LSM9DS1_REGISTER_CTRL_REG2_M         0x21
+#define	LSM9DS1_REGISTER_CTRL_REG3_M         0x22
+#define	LSM9DS1_REGISTER_CTRL_REG4_M         0x23
+#define	LSM9DS1_REGISTER_CTRL_REG5_M         0x24
+#define	LSM9DS1_REGISTER_STATUS_REG_M        0x27
+#define	LSM9DS1_REGISTER_OUT_X_L_M           0x28
+#define	LSM9DS1_REGISTER_OUT_X_H_M           0x29
+#define	LSM9DS1_REGISTER_OUT_Y_L_M           0x2A
+#define	LSM9DS1_REGISTER_OUT_Y_H_M           0x2B
+#define	LSM9DS1_REGISTER_OUT_Z_L_M           0x2C
+#define	LSM9DS1_REGISTER_OUT_Z_H_M           0x2D
+#define	LSM9DS1_REGISTER_CFG_M               0x30
+#define	LSM9DS1_REGISTER_INT_SRC_M           0x31
 
-enum {
-	LSM9DS1_ACCELRANGE_2G                = (0b00 << 3),
-	LSM9DS1_ACCELRANGE_16G               = (0b01 << 3),
-	LSM9DS1_ACCELRANGE_4G                = (0b10 << 3),
-	LSM9DS1_ACCELRANGE_8G                = (0b11 << 3),
-}
 
-enum {
-  LSM9DS1_MAGGAIN_4GAUSS               = (0b00 << 5),  // +/- 4 gauss
-  LSM9DS1_MAGGAIN_8GAUSS               = (0b01 << 5),  // +/- 8 gauss
-  LSM9DS1_MAGGAIN_12GAUSS              = (0b10 << 5),  // +/- 12 gauss
-  LSM9DS1_MAGGAIN_16GAUSS              = (0b11 << 5)   // +/- 16 gauss
-}
 
-enum {
-	LSM9DS1_MAGDATARATE_3_125HZ          = (0b000 << 2),
-	LSM9DS1_MAGDATARATE_6_25HZ           = (0b001 << 2),
-	LSM9DS1_MAGDATARATE_12_5HZ           = (0b010 << 2),
-	LSM9DS1_MAGDATARATE_25HZ             = (0b011 << 2),
-	LSM9DS1_MAGDATARATE_50HZ             = (0b100 << 2),
-	LSM9DS1_MAGDATARATE_100HZ            = (0b101 << 2)
-}
+#define	LSM9DS1_ACCELRANGE_2G                 (0b00 << 3)
+#define	LSM9DS1_ACCELRANGE_16G                (0b01 << 3)
+#define	LSM9DS1_ACCELRANGE_4G                 (0b10 << 3)
+#define	LSM9DS1_ACCELRANGE_8G                 (0b11 << 3)
 
-enum {
-	LSM9DS1_GYROSCALE_245DPS             = (0b00 << 3),  // +/- 245 degrees per second rotation
-	LSM9DS1_GYROSCALE_500DPS             = (0b01 << 3),  // +/- 500 degrees per second rotation
-	LSM9DS1_GYROSCALE_2000DPS            = (0b11 << 3)   // +/- 2000 degrees per second rotation
-}
+
+
+#define LSM9DS1_MAGGAIN_4GAUSS                (0b00 << 5)  // +/- 4 gauss
+#define LSM9DS1_MAGGAIN_8GAUSS                (0b01 << 5)  // +/- 8 gauss
+#define LSM9DS1_MAGGAIN_12GAUSS               (0b10 << 5)  // +/- 12 gauss
+#define LSM9DS1_MAGGAIN_16GAUSS               (0b11 << 5)   // +/- 16 gauss
+
+
+
+#define	LSM9DS1_MAGDATARATE_3_125HZ           (0b000 << 2)
+#define	LSM9DS1_MAGDATARATE_6_25HZ            (0b001 << 2)
+#define	LSM9DS1_MAGDATARATE_12_5HZ            (0b010 << 2)
+#define	LSM9DS1_MAGDATARATE_25HZ              (0b011 << 2)
+#define	LSM9DS1_MAGDATARATE_50HZ              (0b100 << 2)
+#define	LSM9DS1_MAGDATARATE_100HZ             (0b101 << 2)
+
+
+#define	LSM9DS1_GYROSCALE_245DPS              (0b00 << 3),  // +/- 245 degrees per second rotation
+#define	LSM9DS1_GYROSCALE_500DPS              (0b01 << 3),  // +/- 500 degrees per second rotation
+#define	LSM9DS1_GYROSCALE_2000DPS             (0b11 << 3)   // +/- 2000 degrees per second rotation
+
 
 //Structs
 
-struct AccelData {
-	int32_t x, y, z;
-}
+typedef struct {
+	int x;
+    int y;
+    int z;
+} AccelData;
 
-struct MagData {
-	int32_t x, y, z;
-}
+typedef struct {
+	int x;
+    int y;
+    int z;
+} MagData;
 
-struct GyroData {
-	int32_t x, y, z;
-}
+typedef struct {
+	int x;
+    int y;
+    int z;
+} GyroData;
 
-char I2C_Init(unsigned int speed); //init function taking speed as a value in Hz
+char I2C_Init(); //init function taking speed as a value in Hz
 char I2C_InitSensors();
 
 void setupAccel();
