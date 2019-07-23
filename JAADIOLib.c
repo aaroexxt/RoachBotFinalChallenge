14826bef68bc9a3f82cba79f5b4c7373a071e61c
Daniel Lan <daniellan2002@gmail.com>
2019-07-22 23:28:40 -0700

    initial JAADIOLib.h & JAADIOLib.c file
    Updated the JAADIOLib.c and .h

--- JAADIOLib.c
+++ JAADIOLib.c
@@ -4,3 +4,73 @@
  *
  * Created on July 5, 2019, 1:32 PM
  */
+
+
+enum {
+	INPUT,
+	OUTPUT
+}
+
+enum{
+	HIGH,
+	LOW
+}
+
+int setPortDirection(short pinNumber, int direction){
+
+	if (direction == INPUT){
+
+		pinNumber = 1;
+	}
+	else{
+
+		pinNumber = 0;
+	}
+
+}
+
+// setting the port's value to input or output
+
+
+int setPort(short pinNumber, int newValue){
+// set the value to either 1 (high voltage) or 0 (low voltage)
+	if (newValue == HIGH){
+
+		pinNumber = 1;
+	}
+	else{
+
+		pinNumber = 0;
+	}
+}
+
+int readPort(short pinNumber){
+
+	if(pinNumber == 1){
+
+		return 1;
+	}
+	else{
+
+		return 0;
+	}
+
+}
+// read from the input value
+
+
+char checkInputValue(short pinNumber){
+
+	if (pinNumber == 1){
+
+		return 1;
+
+	}
+	else{
+
+		return 0;
+	}
+}
+// check if the input value is an input or output
+
+
--- JAADIOLib.h
+++ JAADIOLib.h
@@ -5,12 +5,48 @@
  * Created on July 22, 2019, 4:58 PM
  */
 
-#ifndef JAADSM_H
-#define	JAADSM_H
+#ifndef JAADIOLIB_H
+#define	JAADIOLIB_H
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
+
+#define SCL_TRIS TRISBbits.TRISB9
+//Serial Clock Pin ID
+
+#define SCL_BIT PORTBbits.RB9
+
+#define SCL_LAT LATBbits.LATB9
+
+
+#define SDA_TRIS TRISBbits.TRISB8
+// Serial Data Pin ID
+#define SDA_BIT PORTBbits.RB8
+
+#define SDA_LAT LATBbits.LATB8
+
+
+int setPortDirection(short pinNumber, char direction);
+// setting the port's value to input or output
+/* example use
+setPortDirection(SCL_TRIS, 0);
+*/
+
+
+int setPort(short pinNumber, char newValue);
+// set the value to either 1 (high voltage) or 0 (low voltage)
+
+
+char readPort(short pinNumber);
+// read from the input value
+
+
+char checkInputValue(short pinNumber);
+// check if the input value is an input or output
+
+
+
 
 
 
@@ -20,4 +56,5 @@ extern "C" {
 #endif
 
 #endif	/* JAADSM_H */
+
 
